fef263ce9a21ea52a0a541301c73d95b461a6991
Your Name <tolgahan.kizilpinar@hotmail.com>
2025-06-06 00:26:43 +0300

    adding KursKayit update operation

--- Controllers/KursController.cs
+++ Controllers/KursController.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using EFCoreApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -25,11 +26,64 @@ namespace EFCoreApp.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Kurs model)
         {
             _context.Kurslar.Add(model);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kurs = await _context.Kurslar.FindAsync(id);
+
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+
+            return View(kurs);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Kurs model)
+        {
+            if (id != model.KursId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Kurslar.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    if (!_context.Kurslar.Any(m => m.KursId == model.KursId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
     }
 }
